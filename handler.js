17f8c9be9e5d0f2ddb8c4aec52b8d665d3479792
ImOskar <gfslason@gmail.com>
2021-02-03 16:00:33 +0000

    added helper function

--- handler.js
+++ handler.js
@@ -2,13 +2,14 @@
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
 const spotify = require('./spotify-api');
+const helper = require('./helper');
 
 module.exports.releases = async event => {
   spotify.getReleases().then(data =>  {
     dynamo.put({
       TableName: process.env.RELEASE_TABLE,
         Item: {
-          itemId: new Date().toString(),
+          itemId: `fri${helper.getFridayNumber()}2021`,
           releases: data
         }
     }).promise();
---
+++ helper.js
@@ -0,0 +1,8 @@
+module.exports.getFridayNumber = () => {
+    let dayMs = 86400000;
+    let firstFridayOf21Ms = new Date(2021, 0, 1).getTime();
+    let todayMs = new Date().getTime();
+    let diffInMs = todayMs - firstFridayOf21Ms;
+    let fridayNumber = Math.floor((diffInMs / dayMs) / 7);
+    return fridayNumber;
+}
