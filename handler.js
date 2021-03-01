9dbba46e4f7d8ae0c685caafa94410c849c69785
ImOskar <gfslason@gmail.com>
2021-03-01 21:04:54 +0000

    fixed return from handler

--- handler.js
+++ handler.js
@@ -1,19 +1,31 @@
-'use strict';
-const AWS = require('aws-sdk');
+"use strict";
+const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
-const spotify = require('./spotify-api');
-const helper = require('./helper');
+const spotify = require("./spotify-api");
+const helper = require("./helper");
 
-module.exports.releases = async event => {
-  spotify.getReleases().then(data =>  {
-    dynamo.put({
-      TableName: process.env.RELEASE_TABLE,
-        Item: {
-          itemId: `fri${helper.getFridayNumber()}2021`,
-          releases: data
-        }
-    }).promise();
-  }).catch(error => {
-    console.log('error: '+ error);
-})
+module.exports.releases = async (event) => {
+  const response = await spotify
+    .getReleases()
+    .then((data) => {
+      return dynamo
+        .put({
+          TableName: process.env.RELEASE_TABLE,
+          Item: {
+            itemId: `fri${helper.getFridayNumber()}2021`,
+            releases: data,
+          },
+        })
+        .promise()
+        .then((data) => {
+          console.log("dynamodb success: " + data);
+        })
+        .catch((error) => {
+          console.log("dynamodb error: " + error);
+        });
+    })
+    .catch((error) => {
+      console.log("error: " + error);
+    });
+  return response;
 };
--- spotify-api.js
+++ spotify-api.js
@@ -1,108 +1,118 @@
-const axios = require("axios")
+const axios = require("axios");
 
 const getAuth = async () => {
-    
-    const headers = {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      auth: {
-        username: process.env.CLIENT_ID,
-        password: process.env.CLIENT_SECRET,
-      },
-    };
-  
-    try {
-      const response = await axios.post(
-        'https://accounts.spotify.com/api/token',
-        'grant_type=client_credentials',
-        headers
-      );
-      return response.data.access_token;
-    } catch (error) {
-      console.log(error);
-    }
+  const headers = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    auth: {
+      username: process.env.CLIENT_ID,
+      password: process.env.CLIENT_SECRET,
+    },
+  };
+
+  try {
+    const response = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      "grant_type=client_credentials",
+      headers
+    );
+    return response.data.access_token;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports.getReleases = async () => {
-    const token = await getAuth();
-    const albums = await getAlbums(token);
-    const singles = await getSingles(token);
-    const releases = [{
-            'type': 'albums',
-            'releases': albums
-            },
-            {
-            'type': 'singles',
-            'releases': singles
-            }];
-    const data = JSON.stringify(releases);
-    return data;
+  const token = await getAuth();
+  const albums = await getAlbums(token);
+  const singles = await getSingles(token);
+  const releases = [
+    {
+      type: "albums",
+      releases: albums,
+    },
+    {
+      type: "singles",
+      releases: singles,
+    },
+  ];
+  return releases;
 };
 
 const extractAlbumData = (data) => {
-    let album = {
-        artist: data.artists[0].name,
-        title: data.name,
-        image: data.images[1].url,
-        url: data.external_urls.spotify,
-        uri: data.uri,
-        tracks: []
-    };
-    return album;
-}
+  let album = {
+    artist: data.artists[0].name,
+    title: data.name,
+    image: data.images[1].url,
+    url: data.external_urls.spotify,
+    uri: data.uri,
+    tracks: [],
+  };
+  return album;
+};
 
-const extractTrackData = (data) => {
-    let track = {
-        artist: data.artists[0].name,
-        title: data.name,
-        image: data.album.images[1].url,
-        url: data.external_urls.spotify,
-        uri: data.uri,
-        preview: data.preview_url
-    };
-    return track;
-}
+const extractTrackData = (data, albumName) => {
+  let track = {
+    artist: data.artists[0].name,
+    title: data.name,
+    image: data.album.images[1].url,
+    url: data.external_urls.spotify,
+    uri: data.uri,
+    preview: data.preview_url,
+  };
+  if (albumName) track.album = albumName;
+  return track;
+};
 
-const addTracksToAlbum = (songArray, artist) => {
-  let albumTracks = songArray.filter(item => item.artist.includes(artist));
+const addTracksToAlbum = (songArray, uri) => {
+  let albumTracks = songArray.filter((item) => item.album.includes(uri));
   return albumTracks;
-}
+};
 
 const getAlbums = async (token) => {
   const data = await getPlaylistItems(token, process.env.ALBUM_URL, []);
-  
-  let albums = data.map(obj => extractAlbumData(obj.track.album));
-  let tracks = data.map(obj => extractTrackData(obj.track));
-  let filteredAlbums = [...new Map(albums.map(obj => [JSON.stringify(obj), obj])).values()];
-  filteredAlbums.map(item => item.tracks = addTracksToAlbum(tracks, item.artist));
+  let albums = data.map((obj) => extractAlbumData(obj.track.album));
+  let tracks = data.map((obj) =>
+    extractTrackData(obj.track, obj.track.album.uri)
+  );
+  let filteredAlbums = [
+    ...new Map(albums.map((obj) => [JSON.stringify(obj), obj])).values(),
+  ];
+  filteredAlbums.map(
+    (item) => (item.tracks = addTracksToAlbum(tracks, item.uri))
+  );
   return filteredAlbums;
-}
+};
 
 const getSingles = async (token) => {
   const data = await getPlaylistItems(token, process.env.SINGLES_URL, []);
-  let tracks = data.map(obj => extractTrackData(obj.track));
-  let filteredTracks = [...new Map(tracks.map(obj => [JSON.stringify(obj), obj])).values()];
+  let tracks = data.map((obj) => extractTrackData(obj.track));
+  let filteredTracks = [
+    ...new Map(tracks.map((obj) => [JSON.stringify(obj), obj])).values(),
+  ];
   return filteredTracks;
-}
+};
 
 async function getPlaylistItems(token, url, data) {
   let options = {
     url: url,
     headers: {
-         'Authorization': 'Bearer ' + token
-     }};
+      Authorization: "Bearer " + token,
+    },
+  };
 
   const dataArray = data;
-  await axios(options).then(response => {
-    dataArray.push(...response.data.items);
-    if (response.data.next) {
-      return getPlaylistItems(token, response.data.next, dataArray);
-    }
-  })
-  .catch(error => {
-      console.log('error: '+ error);
-  })
+  await axios(options)
+    .then((response) => {
+      dataArray.push(...response.data.items);
+      if (response.data.next) {
+        return getPlaylistItems(token, response.data.next, dataArray);
+      }
+    })
+    .catch((error) => {
+      console.log("error: " + error);
+    });
   return dataArray;
 }
